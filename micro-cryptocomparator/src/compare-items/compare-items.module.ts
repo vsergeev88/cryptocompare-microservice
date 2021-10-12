0825884b545557404126cc64b5fdabf5a15153f0
Valentin Sergeev <valentin_sergeev@epam.com>
2021-10-12 12:08:23 +0300

    small fixes

--- micro-cryptocomparator/src/compare-items/compare-items.module.ts
+++ micro-cryptocomparator/src/compare-items/compare-items.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { CompareItem } from './compare-item.model';
-// import { UsersController } from './users.controller';
 import { CompareItemsService } from './compare-items.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([CompareItem])],
   providers: [CompareItemsService],
-  // controllers: [UsersController],
 })
 export class CompareItemsModule {}
--- micro-cryptocomparator/src/constants/payload-default.ts
+++ micro-cryptocomparator/src/constants/payload-default.ts
@@ -1,4 +1,6 @@
-export const payloadDefault = {
+import { CompareRequestPayload } from '../types';
+
+export const payloadDefault: CompareRequestPayload = {
   fsyms: ['BTC', 'XRP', 'ETH', 'BCH', 'EOS', 'LTC', 'XMR', 'DASH'],
   tsyms: ['USD', 'EUR', 'GBP', 'JPY', 'RUR'],
 };
